fad0d43c3545e9e921a3467a81b083e228632192
JIVitian <jivitian@gmail.com>
2022-08-07 17:42:03 -0300

    Delete pokemon of the list working

--- src/app/pages/pokemon-index/pokemon-index.component.ts
+++ src/app/pages/pokemon-index/pokemon-index.component.ts
@@ -6,23 +6,38 @@ import { PokemonService } from 'src/app/services/pokemon-service/pokemon.service
 @Component({
   selector: 'app-pokemon-index',
   templateUrl: './pokemon-index.component.html',
-  styleUrls: ['./pokemon-index.component.scss']
+  styleUrls: ['./pokemon-index.component.scss'],
 })
 export class PokemonIndexComponent implements OnInit {
   pokemonList$: Observable<Pokemon[]>;
 
-  constructor(private pokemonService: PokemonService) { }
+  constructor(private pokemonService: PokemonService) {}
 
   ngOnInit(): void {
-    this.pokemonList$ = this.pokemonService.getPokemons().pipe(
-      map((pokemons: Pokemon[]) => {
-        return pokemons.map((pokemon) => new Pokemon(pokemon));
-      })
-    );
+    this.udateList();
   }
 
   alert(action: string) {
     alert(action);
   }
 
+  deletePokemon(id: number) {
+    this.pokemonService.deletePokemon(id).subscribe({
+      next: () => {
+        this.alert('Pokemon deletado com sucesso!');
+        this.udateList();
+      },
+      error: () => {
+        this.alert('Erro ao deletar o Pokemon!');
+      },
+    });
+  }
+
+  udateList() {
+    this.pokemonList$ = this.pokemonService.getPokemons().pipe(
+      map((pokemons: Pokemon[]) => {
+        return pokemons.map((pokemon) => new Pokemon(pokemon));
+      })
+    );
+  }
 }
