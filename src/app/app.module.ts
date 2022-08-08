5e52fdff54ea7019865adf60fde1bde46cb696c9
JIVitian <jivitian@gmail.com>
2022-08-08 01:41:15 -0300

    Moving components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PokemonIndexComponent } from './pages/pokemon-index/pokemon-index.component';
-import { PokemonSearchComponent } from './pages/pokemon-index/components/pokemon-search/pokemon-search.component';
-import { PokemonListComponent } from './pages/pokemon-index/components/pokemon-list/pokemon-list.component';
-import { PokemonEditorComponent } from './pages/pokemon-index/components/pokemon-editor/pokemon-editor.component';
-import { CustomRangeComponent } from './components/custom-range/custom-range.component';
+import { PokemonEditorComponent } from './components/pokemon-editor/pokemon-editor.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { PokemonUpdateComponent } from './pages/pokemon-update/pokemon-update.component';
 import { PokemonCreateComponent } from './pages/pokemon-create/pokemon-create.component';
+import { PokemonSearchComponent } from './components/pokemon-search/pokemon-search.component';
+import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +19,6 @@ import { PokemonCreateComponent } from './pages/pokemon-create/pokemon-create.co
     PokemonSearchComponent,
     PokemonListComponent,
     PokemonEditorComponent,
-    CustomRangeComponent,
     PokemonUpdateComponent,
     PokemonCreateComponent
   ],
